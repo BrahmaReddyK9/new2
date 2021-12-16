c71e22abd85197249e1e509760e22cfcb556e2af
BrahmaReddyK9 <brahmareddykurri9@gmail.com>
2021-12-16 19:00:51 +0530

    adding

---
+++ src/test/java/seleniumbrowser/webapptest.java
@@ -0,0 +1,20 @@
+package seleniumbrowser;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class webapptest {
+	@Test
+	public <App> void testlogin1() {
+		webapp myapp = new webapp();
+		Assert.assertEquals(0, myapp.userLogin("abc", "abc123"));
+	}
+
+	@Test
+	public <App> void testloin2() {
+		webapp myapp = new webapp();
+		Assert.assertEquals(1, myapp.userLogin("abc", "abc@123"));
+	}
+}
+
+
